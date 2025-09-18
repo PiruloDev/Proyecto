10f0ccf33e275f7265bcd1b61fe22a580e93353b
AndresAlcala <pipealcala22@gmail.com>
2025-09-18 16:52:12 -0500

    Implementar Manejo Endpoints Pedidos

---
+++ Proyecto Modulo Pedidos/Config.php
@@ -0,0 +1,18 @@
+<?php
+//Pedidos API
+define('API_BASE_URL', 'http://localhost:8080');
+define('ENDPOINT_PEDIDOS', '/pedidos');
+define('ENDPOINT_POST_PEDIDOS', '/pedidos');
+define('ENDPOINT_PUT_PEDIDOS', '/pedidos');
+define('ENDPOINT_DELETE_PEDIDOS', '/pedidos');
+//Estados Pedidos API
+define('ENDPOINT_ESTADOS_PEDIDOS', '/estadosPedidos');
+define('ENDPOINT_POST_ESTADOS_PEDIDOS', '/estadosPedidos');
+define('ENDPOINT_PUT_ESTADOS_PEDIDOS', '/estadosPedidos');
+define('ENDPOINT_DELETE_ESTADOS_PEDIDOS', '/estadosPedidos');
+//Pedidos Proveedores API
+define('ENDPOINT_PEDIDOS_PROVEEDORES', '/pedidosproveedores');
+define('ENDPOINT_POST_PEDIDOS_PROVEEDORES', '/pedidosproveedores');
+define('ENDPOINT_PUT_PEDIDOS_PROVEEDORES', '/pedidosproveedores');
+define('ENDPOINT_DELETE_PEDIDOS_PROVEEDORES', '/pedidosproveedores');
+?>
