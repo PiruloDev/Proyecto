3ae7644f9d3d0c5fbe9c39ff4c8ce637cc21e88d
anaasofiaa <sofiaaporraas@gmail.com>
2025-10-01 17:09:01 -0500

    Reestablecimiento backend de Reportes

--- API (SpringBoot)/Proyecto/src/main/java/com/example/Proyecto/controller/UsuariosRegistradosController.java
+++ API (SpringBoot)/Proyecto/src/main/java/com/example/Proyecto/controller/UsuariosRegistradosController.java
@@ -1,22 +1,20 @@
 package com.example.Proyecto.controller;
 
-import com.example.Proyecto.dto.UsuariosRegistradosDTO;
+import com.example.Proyecto.model.UsuariosRegistradosDTO;
 import com.example.Proyecto.service.UsuariosRegistradosService.UsuariosRegistradosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios")
 public class UsuariosRegistradosController {
 
     @Autowired
     private UsuariosRegistradosService usuariosRegistradosService;
 
-    @GetMapping("/reporte")
+    @GetMapping("/reporte/usuarios")
     public List<UsuariosRegistradosDTO> obtenerReporteUsuarios() {
         return usuariosRegistradosService.obtenerUsuariosRegistrados();
     }
