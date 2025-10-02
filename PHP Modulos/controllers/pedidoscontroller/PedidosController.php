11dd77d0e5d587c53ad006e911f8e7b5db4ebdba
AndresAlcala <pipealcala22@gmail.com>
2025-10-01 22:24:53 -0500

    Vista corregida pedidos

--- PHP Modulos/controllers/pedidoscontroller/PedidosController.php
+++ PHP Modulos/controllers/pedidoscontroller/PedidosController.php
@@ -44,7 +44,7 @@ class PedidosController {
         
         $pedidos = $this->pedidosService->obtenerPedidos();
 
-        require __DIR__ . '/../../views/pedidosviews/index.php';
+        require __DIR__ . '/../../views/pedidosviews/pedidos.php';
     }   
     
     private function getPostData(array $keys) {
