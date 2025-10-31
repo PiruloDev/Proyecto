659aedfb043c29e014bb83329a7f5f523014659c
PiruloDev <jimeneztrianabrayan@gmail.com>
2025-10-31 15:16:10 -0500

    Organizacion

--- PanCode/app/src/main/java/com/example/pancode/MainActivity.kt
+++ PanCode/app/src/main/java/com/example/pancode/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.pancode
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.pancode.Navigation.AppNavigation
+import com.example.pancode.Navigation.NavigationUsers.AppNavigation
 import com.example.pancode.ui.theme.PanCodeTheme
 
 class MainActivity : ComponentActivity() {
