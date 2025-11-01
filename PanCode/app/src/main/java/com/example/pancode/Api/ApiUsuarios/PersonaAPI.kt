c6b36578665a8d1bd4eea53c3a2c3e69cb08a7e5
PiruloDev <jimeneztrianabrayan@gmail.com>
2025-11-01 02:48:11 -0500

    Api Usuarios

---
+++ PanCode/app/src/main/java/com/example/pancode/Api/ApiUsuarios/PersonaAPI.kt
@@ -0,0 +1,8 @@
+package com.example.pancode.Api.ApiUsuarios
+
+data class PersonaAPI(
+    var nombre: String = "",
+    var email: String = "",
+    var telefono: String = "",
+    var contrasena: String = ""
+)
