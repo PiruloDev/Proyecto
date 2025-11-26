5c17e3ce50f093234ce0b8a2f38efaa0fd3f309c
PiruloDev <jimeneztrianabrayan@gmail.com>
2025-11-26 16:48:42 -0500

    Creacion de Arquitectura de Laravel

--- PHP Modulos/Empleadoindex.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/controllers/userscontroller/obtenerEmpleadoController.php';
-$controller = new ObtenerEmpleadoController();
-$controller->manejoPeticionEmpleado();
-?>
--- PHP Modulos/Userindex.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/controllers/userscontroller/obtenerClienteController.php';
-$controller = new ObtenerClienteController();
-$controller->manejoPeticionCliente();
-?>
