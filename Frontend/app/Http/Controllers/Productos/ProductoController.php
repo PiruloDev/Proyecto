9a1319a8320cfa1f65f1f0f8063ee36f1be0bf7d
anaasofiaa <sofiaaporraas@gmail.com>
2026-03-20 10:37:51 -0500

    Corrección de IP en Reportes y Productos

--- Frontend/app/Http/Controllers/Productos/ProductoController.php
+++ Frontend/app/Http/Controllers/Productos/ProductoController.php
@@ -9,7 +9,13 @@ use Illuminate\Support\Facades\Log;
 
 class ProductoController extends Controller
 {
-    protected $apiUrl = 'http://localhost:8080/productos';
+   protected $baseUrl;
+
+    public function __construct()
+    {
+        // Obtiene la URL base de la API desde el archivo .env
+        $this->baseUrl = env('API_BASE_URL', 'http://32.193.167.191:8080'); 
+    }
 
     public function index()
     {
