1d35b56c4363f9ec61e30cbbd93dbd3b6aa86d9a
anaasofiaa <sofiaaporraas@gmail.com>
2026-03-23 01:27:44 -0500

    Arreglo IP en Reportes

--- Frontend/app/Services/Reportes/OrdenSalidaService.php
+++ Frontend/app/Services/Reportes/OrdenSalidaService.php
@@ -11,9 +11,9 @@ class OrdenSalidaService {
 
     protected $apiService;
 
-    public function __construct(ApiService $apiService)
+    public function __construct()
     {
-        $this->apiService = $apiService;
+        $this->baseUrl = env('API_BASE_URL', 'http://32.193.167.191:8080'); 
     }
 
     public function obtenerVentas() {
