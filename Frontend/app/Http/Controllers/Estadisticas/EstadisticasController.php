b7121d0ff3eee07583f386fd096f460d5e81a54f
PiruloDev <jimeneztrianabrayan@gmail.com>
2026-03-25 19:32:13 -0500

    fixes

--- Frontend/app/Http/Controllers/Estadisticas/EstadisticasController.php
+++ Frontend/app/Http/Controllers/Estadisticas/EstadisticasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Estadisticas;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use App\Services\Reportes\ProductosMasVendidosService;
 use App\Services\Reportes\UsuariosRegistradosService;
 
